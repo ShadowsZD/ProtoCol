4970f8313e20557b23b974ba73d9e587d8c30c7a
ShadowsZD <kartkork@gmail.com>
2017-09-21 22:33:33 -0300

    Packet changes
    Some changes on packet

--- Protocol.h
+++ Protocol.h
@@ -9,13 +9,15 @@
 #include <linux/if.h>
 
 #define buf_max 65536
-#define framing_bits 0b11000011
+#define framing_bits 0b01111110  //CONFIRA:
 #define frame_n 8
 
 typedef struct {
-	int size:4;
-	int seq:;
+	int size:4;     //acho q isso aq era pra ser 5 ou eu to cego
+	int seq:6;
+	int type:5;
 	unsigned char* data;
+	int parity:8; //?
 } packet;
 
 #endif // PROTOCOL_H
